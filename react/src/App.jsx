85d885b825655ee41348d5bc0c31e4223ecd79b9
johnson-713 <johnsonelangovan@gmail.com>
2024-03-19 22:29:47 +0530

    ref vs state

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,26 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { data } from './data'
 import Card from './Card'
+import { useRef, useState } from 'react'
 
 function App() {
+  // const [selectedCard, setSelectedCard] = useState({})
+  const [count, setCount] = useState(0)  //state change (mutable) refresh (state -> initial state)
+  const countRef = useRef(0)
+     // ref -> 1 -> nextpage -> 1 -> future contents
 
   const filteredData = data.filter((i) => i.age > 20)
 
   const handleclick = () => {
-    alert("Hi!!!!")
+    setCount(count+1)
+    alert(`no of count ${count}`)
+
+    console.log(countRef)
   }
+
+  
 
   return (
     <div>
       {
         data.length > 0 ? (
           <div>
-            {filteredData.map((i, ind) => (
+            {filteredData.map((i, index) => (
             <Card 
-              key={ind}
+              key={index}
               image={i.img}
               name={i.name}
               age={i.age}
